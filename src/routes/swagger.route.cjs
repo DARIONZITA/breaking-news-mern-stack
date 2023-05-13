3091889e7ec843408a91a454b7a7839ae3459d62
Dário Zongo Garcia Nzita <92880647+DARIONZITA@users.noreply.github.com>
2023-05-13 18:43:10 +0100

    update

---
+++ src/routes/swagger.route.cjs
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const swaggerUiExpress = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
+
+
+router.use("/",swaggerUiExpress.serve)
+router.get('/',swaggerUiExpress.setup(swaggerDocument))
+export default router
--- src/routes/swagger.route.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express'
-import swaggerUiExpress from 'swagger-ui-express'
-import swaggerDocument from '../../swagger.json' assert { type: "json"}
-const route = Router()
-
-route.use("/",swaggerUiExpress.serve)
-route.get('/',swaggerUiExpress.setup(swaggerDocument))
-export default route
